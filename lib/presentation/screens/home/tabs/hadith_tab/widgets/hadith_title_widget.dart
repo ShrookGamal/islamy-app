3e07076b986aabdcb90ea1c8e900b1acdaf67e72
Shrouk <gshrouk340@gmail.com>
2024-10-20 17:38:14 +0300

    + splash screen + quran tab + quran details + hadith tab + hadith details

--- lib/presentation/screens/home/tabs/hadith_tab/widgets/hadith_title_widget.dart
+++ lib/presentation/screens/home/tabs/hadith_tab/widgets/hadith_title_widget.dart
@@ -3,10 +3,10 @@ import 'package:flutter_projects/core/routes_manager.dart';
 
 import '../hadith_tab.dart';
 
-class hadithTitleWidget extends StatelessWidget {
+class HadithTitleWidget extends StatelessWidget {
   Hadith hadith;
 
-  hadithTitleWidget({super.key, required this.hadith});
+  HadithTitleWidget({super.key, required this.hadith});
 
   @override
   Widget build(BuildContext context) {
